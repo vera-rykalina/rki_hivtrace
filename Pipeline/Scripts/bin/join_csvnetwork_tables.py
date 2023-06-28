d6f17fb7de1813ae8f753755db19aed9c7f9be0b
vera-rykalina <verarykalina@gmail.com>
2023-06-28 18:39:29 +0200

    Add a script to join tables

--- Pipeline/Scripts/bin/join_csvnetwork_tables.py
+++ Pipeline/Scripts/bin/join_csvnetwork_tables.py
@@ -7,17 +7,18 @@ import pandas as pd
 # Create a list of dfs
 dfs = []
 
-for infilename in sys.args[1:]:
-    newcolname = infilename.rsplit("/")[-1]
+for infilename in sys.argv[1:]:
+    newcolname = infilename.rsplit("/")[-1].split("_")[0]
+    print(newcolname)
     dfs.append(pd.read_csv(infilename, sep = ",", index_col = False))
+    # Rename column names to have file names as column names
+    for df in dfs:
+        df.rename({"ClusterID": newcolname}, axis = 1, inplace = True)
 
-# Rename column name to be the file name
-for df in dfs:
-    df = df.rename(columns={"ClusterID":"newcolname"})
 
 df = dfs[0]
 for df_ in dfs[1:]:
     df = df.merge(df_, on = "SequenceID", how = "inner")
 
 # Create clean output csv
-df.to_csv = ("joined_csvnetwork_tables" + ".csv", sep = ",", index = False, encoding = "utf-8")
+df = df.to_csv("joined_csvnetwork_tables" + ".csv", sep = ",", index = False, encoding = "utf-8")
