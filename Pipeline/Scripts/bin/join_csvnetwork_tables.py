21f8b290bf22112d1e4d6d351e4139e6ae17c15d
vera-rykalina <verarykalina@gmail.com>
2023-07-12 15:06:49 +0200

    Change join to outer, replace NaN to zeto

--- Pipeline/Scripts/bin/join_csvnetwork_tables.py
+++ Pipeline/Scripts/bin/join_csvnetwork_tables.py
@@ -9,7 +9,6 @@ dfs = []
 
 for infilename in sys.argv[1:]:
     newcolname = infilename.rsplit("/")[-1].split("_")[0]
-    print(newcolname)
     dfs.append(pd.read_csv(infilename, sep = ",", index_col = False))
     # Rename column names to have file names as column names
     for df in dfs:
@@ -18,7 +17,10 @@ for infilename in sys.argv[1:]:
 # Merge dfs
 df = dfs[0]
 for df_ in dfs[1:]:
-    df = df.merge(df_, on = "SequenceID", how = "inner")
+    df = df.merge(df_, on = "SequenceID", how = "outer")
+
+# Replace empty cells with zeros
+df.fillna(0, inplace=True, downcast="infer")
 
 # Create clean output csv
 df = df.to_csv("joined_csvnetwork_tables" + ".csv", sep = ",", index = False, encoding = "utf-8")
